ce379996631c60b1dca8b0c8aac87aa1ab4f4c68
Typas Liao <typascake@gmail.com>
2022-03-21 06:32:32 +0800

    not really satisfied but useable

--- wezterm.lua
+++ wezterm.lua
@@ -2,6 +2,43 @@ local wezterm = require 'wezterm';
 
 return {
   color_scheme = "OneHalfLight",
+
+  -- tab bar appearance --
+  -- based on macos terminal --
+  hide_tab_bar_if_only_one_tab = true,
+  use_fancy_tab_bar = false,
+  colors = {
+    tab_bar = {
+      background = "#bbbbbb",
+
+      active_tab = {
+        bg_color = "#cfcfcf",
+        fg_color = "#262626",
+      },
+
+      inactive_tab = {
+        bg_color = "#bbbbbb",
+        fg_color = "#3f3f3f",
+      },
+
+      inactive_tab_hover = {
+        bg_color = "#a8a8a8",
+        fg_color = "#272727",
+      },
+
+      new_tab = {
+        bg_color = "#bbbbbb",
+        fg_color = "#555555",
+      },
+
+      new_tab_hover = {
+        bg_color = "#a8a8a8",
+        fg_color = "#444444",
+      }
+    }
+  },
+
+  -- fonts --
   font_size = 12.0,
   font = wezterm.font_with_fallback({
       {
@@ -10,7 +47,17 @@ return {
         harfbuzz_features={"ss05", "ss03", "ss02", "ss08", "ss06", "cv02", "cv10", "cv16"}
       },
       "Noto Sans Mono CJK TC",
-      "Noto Sans",
-      "Noto Color Emoji"
+      "Noto Sans Mono",
   }),
+  font_rules = {
+    {
+      italic = true,
+      font = wezterm.font_with_fallback({
+          {
+            family="SF Mono",
+            style="Italic",
+          },
+      }),
+    },
+  },
 }
