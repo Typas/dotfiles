956ec0dd564a7ccb0fcd73ecb507e1da6fea593a
Typas Liao <typascake@gmail.com>
2022-03-29 05:08:29 +0800

    update to match texmf

--- snippets/c-mode/.yas-compiled-snippets.el
+++
@@ -1,2 +0,0 @@
-;;; Compiled snippets and support files for `c-mode'
-;;; Do not edit! File generated at Sun Mar 27 07:58:53 2022
--- snippets/rustic-mode/.yas-compiled-snippets.el
+++
@@ -1,2 +0,0 @@
-;;; Compiled snippets and support files for `rustic-mode'
-;;; Do not edit! File generated at Sun Mar 27 07:58:53 2022
