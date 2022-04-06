ec948144f6b28c5b6d0634669de1e5bc5aa7f610
Typas Liao <typascake@gmail.com>
2022-04-06 22:38:55 +0800

    basic settings

---
+++ lua/basic/keybinds.lua
@@ -0,0 +1,30 @@
+
+-- leader key set to space
+vim.g.mapleader = " "
+
+vim.keybinds = {
+  gmap = vim.api.nvim_set_keymap,
+  bmap = vim.api.nvim_buf_set_keymap,
+  dgmap = vim.api.nvim_del_keymap,
+  dbmap = vim.api.nvim_buf_del_keymap,
+  opts = {noremap = true},
+}
+
+vim.keybinds.gmap("n", "<ESC>", ":nohlsearch<CR>", vim.keybinds.opts)
+
+-- map keybinds to fit doom emacs
+vim.keybinds.gmap("n", "<leader>qq", ":qa<CR>", vim.keybinds.opts)
+vim.keybinds.gmap("n", "<leader>wv", ":vsp<CR>", vim.keybinds.opts)
+vim.keybinds.gmap("n", "<leader>ws", ":sp<CR>", vim.keybinds.opts)
+vim.keybinds.gmap("n", "<leader>w", "<C-w>", vim.keybinds.opts)
+
+-- packer commands
+vim.keybinds.gmap("n", "<leader>Pc", ":PackerCompile<CR>", vim.keybinds.opts)
+vim.keybinds.gmap("n", "<leader>Pu", ":PackerUpdate<CR>", vim.keybinds.opts)
+vim.keybinds.gmap("n", "<leader>Ps", ":PackerSync<CR>", vim.keybinds.opts)
+
+-- telescope commands
+vim.keybinds.gmap("n", "<leader>ff", ":Telescope find_files<CR>", vim.keybinds.opts)
+vim.keybinds.gmap("n", "<leader>fg", ":Telescope live_grep<CR>", vim.keybinds.opts)
+vim.keybinds.gmap("n", "<leader>fb", ":Telescope buffers<CR>", vim.keybinds.opts)
+vim.keybinds.gmap("n", "<leader>fh", ":Telescope help_tags<CR>", vim.keybinds.opts)
---
+++ lua/basic/plugins.lua
@@ -0,0 +1,73 @@
+---@diagnostic disable: undefined-global
+-- https://github.com/wbthomason/packer.nvim
+
+local packer = require("packer")
+packer.startup(
+{
+  function()
+    use "wbthomason/packer.nvim"
+
+    use {
+      "rmehri01/onenord.nvim",
+      config = function()
+        require("onenord").setup({
+          borders = true,
+          styles = {
+            comments = "italic",
+            strings = "NONE",
+            keywords = "NONE",
+            functions = "NONE",
+            variables = "NONE",
+          },
+        })
+      end
+    }
+
+    use {
+      "lewis6991/gitsigns.nvim",
+      config = function()
+        require("gitsigns").setup()
+      end
+    }
+
+    use {
+      "numToStr/Comment.nvim",
+      config = function()
+        require("Comment").setup({
+          mappings = {
+            extra = false,
+          }
+        })
+      end
+    }
+
+    use {
+      "nvim-treesitter/nvim-treesitter",
+      run = ":TSUpdate",
+    }
+
+    use "gpanders/editorconfig.nvim"
+
+    use {
+      "folke/which-key.nvim",
+    }
+
+    use {
+      "nvim-telescope/telescope.nvim",
+    }
+
+    use "nvim-lua/plenary.nvim"
+
+    use {
+      "nvim-lualine/lualine.nvim",
+      requires = { "kyazdani42/nvim-web-devicons", opt = true },
+      config = function()
+        require("lualine").setup()
+      end
+    }
+
+  end
+}
+)
+
+return packer
