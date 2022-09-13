29553c17c1e045be07204c0d5e265614ac230267
Typas Liao <typascake@gmail.com>
2022-09-13 21:03:16 +0800

    reorder package list, add -y

--- fedora-init.sh
+++ fedora-init.sh
@@ -6,7 +6,7 @@ bash "$LOCATION/rust-analyzer-linux-update.sh"
 
 while read -r line
 do
-sudo dnf install "$line"
+sudo dnf install -y "$line"
 done < "$LOCATION/package.list"
 
 bash "$LOCATION/common-init.sh"
