0d70d1977a5f0b16f4e1535be70ed279ba7d2d68
Typas Liao <typascake@gmail.com>
2026-04-12 14:35:11 +0800

    fix: add ubuntu to shell init

--- scripts/shell-init.sh
+++ scripts/shell-init.sh
@@ -5,6 +5,6 @@ D_LOC="${D_LOC:?D_LOC must be set}"
 os="${1:?usage: shell-init.sh <os>}"
 
 case "$os" in
-    mac)                             export D_LOC; zsh "$D_LOC/scripts/zinit-install.sh" ;;
-    fedora|opensuse*|cachyos|debian) export D_LOC; bash "$D_LOC/scripts/oh-my-bash-install.sh" ;;
+    mac)                                    export D_LOC; zsh "$D_LOC/scripts/zinit-install.sh" ;;
+    fedora|opensuse*|cachyos|debian|ubuntu) export D_LOC; bash "$D_LOC/scripts/oh-my-bash-install.sh" ;;
 esac
